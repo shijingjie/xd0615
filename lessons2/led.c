ed52f7ec4955a876e515913141962145526b8a48
shijingjie <66952492+shijingjie@users.noreply.github.com>
2020-06-16 20:57:48 +0800

    Update led.c

--- lessons2/led.c
+++ lessons2/led.c
@@ -12,18 +12,18 @@ void loop()
   {
     digitalWrite(i, HIGH);
     digitalWrite(i+1, HIGH);
-    delay(500); // Wait for 1000 millisecond(s)
+    delay(500); // Wait for 500 millisecond(s)
     digitalWrite(i, LOW);
     digitalWrite(i+1, LOW);
-    delay(500); // Wait for 1000 millisecond(s)
-  }
+    delay(500); // Wait for 500 millisecond(s)
+  }//两个两个地亮，如0、1亮，2、3亮、4、5亮以此类推
   for(i=0;i<=5;i++)
   {
     digitalWrite(i, HIGH);
     digitalWrite(i+2, HIGH);
-    delay(500); // Wait for 1000 millisecond(s)
+    delay(500); // Wait for 500 millisecond(s)
     digitalWrite(i, LOW);
     digitalWrite(i+2, LOW);
-    delay(500); // Wait for 1000 millisecond(s)
-  }
+    delay(500); // Wait for 500 millisecond(s)
+  }//交替着亮，如0、2亮，1、3亮、2、4亮以此类推
 }
